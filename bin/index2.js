140756590d6dc883028bb80989a1bb2b845b9f8d
kanitmann <kanitmann90@gmail.com>
2022-06-01 18:24:47 +0530

    CLI CSV invput (1/2)

--- bin/index2.js
+++ bin/index2.js
@@ -1,10 +1,12 @@
 #! /usr/bin/env node
-
 import chalk from "chalk";
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import pretty from "pretty";
 import figlet from "figlet";
+import inquirer from "inquirer";
+import {parse} from "csv-parse";
+import fs from "fs";
 
 // import reads from "./read.js"
 console.log(
@@ -15,11 +17,29 @@ console.log(
 
 console.log(chalk.bgRed("Welcome to ") + chalk.black(chalk.bgBlue("Destro")));
 
+let pathName = "";
+
+async function readCSV() {
+  fs.createReadStream("./bin/" + pathName)
+      .pipe(parse({ delimiter: ",", from_line: 2 }))
+      .on("data", function (row) {
+          console.log(row);
+      })
+      .on("end", function () {
+          console.log("Finished reading csv");
+      })
+      .on("error", function (error) {
+          console.log(error.message);
+      });
+}
 
 const y = yargs()
 y.version('v1.0.1')
 
+const argv = process.argv.slice(2);
+
 y.command({
+  usage: 'Usage: destro <command> [options]',
   command: '-i <filename> <distribution>',
   describe: 'tells if the version is greater than or equal to the version specified or not. \n',
   builder: {
@@ -49,10 +69,25 @@ y.command({
       type: 'string'
     }
   },
-  handler(argv) {
+  handler(argv) { 
     notes.removeNote(argv.title)
   } 
 })
+
+if(argv[0] == "-i")
+{
+  if(argv[1] == "-update")
+  {
+    console.log("Update Working");
+    pathName = argv[2];
+  }
+  else
+  {
+    pathName = argv[1];
+  }
+  // await takeCSV();
+  await readCSV();
+}
 
 y.parse(process.argv.slice(2))
 

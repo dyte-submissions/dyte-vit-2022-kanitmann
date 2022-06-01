d4fa7401c27888309ce2163b24ddd5ce9745c4bf
kanitmann <kanitmann90@gmail.com>
2022-06-01 18:49:11 +0530

    Secret Added :smile:

--- bin/index2.js
+++ bin/index2.js
@@ -7,17 +7,63 @@ import figlet from "figlet";
 import inquirer from "inquirer";
 import {parse} from "csv-parse";
 import fs from "fs";
+import CliFrames from 'cli-frames';
+
 
 // import reads from "./read.js"
 console.log(
   chalk.yellow(
-    figlet.textSync('Index2', { horizontalLayout: 'full' })
+    figlet.textSync('Index', { horizontalLayout: 'full' })
   )
 );
 
 console.log(chalk.bgRed("Welcome to Destro"));
 
 let pathName = "";
+let distName = "";
+
+function animation1()
+{
+  const frames = [
+    "╔════╤╤╤╤════╗\n" +
+    "║    │││ \\   ║\n" +
+    "║    │││  O  ║\n" +
+    "║    OOO     ║",
+
+    "╔════╤╤╤╤════╗\n" +
+    "║    ││││    ║\n" +
+    "║    ││││    ║\n" +
+    "║    OOOO    ║",
+
+    "╔════╤╤╤╤════╗\n" +
+    "║   / │││    ║\n" +
+    "║  O  │││    ║\n" +
+    "║     OOO    ║",
+
+    "╔════╤╤╤╤════╗\n" +
+    "║    ││││    ║\n" +
+    "║    ││││    ║\n" +
+    "║    OOOO    ║"
+];
+
+
+// Load frames when creating the instance
+new CliFrames({
+    frames: ["3", "2", "1"]
+  , autostart: {
+        delay: 1000
+      , end: function (err, data) {
+            // Create another animation
+            var animation = new CliFrames();
+            animation.load(frames);
+            animation.start({
+                repeat: true
+              , delay: 250
+            });
+        }
+    }
+});
+}
 
 async function readCSV() {
   fs.createReadStream("./bin/" + pathName)
@@ -74,19 +120,40 @@ y.command({
   } 
 })
 
+y.command({
+  command: '-animation',
+  describe: 'Side fun animation in CLI\n',
+  builder: {
+    title: {
+      describe: '   ',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler(argv) { 
+    notes.removeNote(argv.title)
+  } 
+})
+
 if(argv[0] == "-i")
 {
   if(argv[1] == "-update")
   {
     console.log("Update Working");
     pathName = argv[2];
+    distName = argv[3];
   }
   else
   {
     pathName = argv[1];
+    distName = argv[2];
   }
   // await takeCSV();
   await readCSV();
+}
+else if(argv[0] == "-animation")
+{
+  animation1();
 }
 
 y.parse(process.argv.slice(2))
